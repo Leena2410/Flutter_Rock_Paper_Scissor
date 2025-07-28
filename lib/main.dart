c9f280be6a09d7cbc71f3370ab5f8be6ea14e1de
Leena <emadeldeen.lina@gmail.com>
2025-07-28 19:29:10 +0300

    Basic full project

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,225 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: non_constant_identifier_names, avoid_unnecessary_containers
 
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'dart:developer' as dev;
+
+/* ******************* VARIABLES ******************* */
+var appbar_bg_color = Color.fromARGB(255, 148, 148, 255);
+var appbar_text_Color = Color.fromARGB(255, 82, 82, 152);
+var page_bg_color = Color.fromARGB(255, 255, 255, 255);
+
+var user_choise = "img/unidentified.png";
+var my_choise = "img/unidentified.png";
+var result = "VS";
+
+/* ******************* VARIABLES ******************* */
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Homewidget(),
     );
+  }
+  Widget Homewidget(){
+    return Scaffold(
+      backgroundColor: page_bg_color,
+      appBar: AppBar(
+        backgroundColor: appbar_bg_color,
+        title: Text(
+          "Rock-Paper_Scisser",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: appbar_text_Color,
+          ),
+        ),
+      ),
+      body: Column(
+        // mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 20,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                color: appbar_bg_color,
+                width: 100,
+                height: 120,
+                child: Image.asset(user_choise, ),
+              ),
+              Container(
+                child: Text(
+                  result,
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    color: appbar_bg_color,
+                  ),
+                ),
+              ),
+              Container(
+                color: appbar_bg_color,
+                width: 100,
+                height: 120,
+                child: Image.asset(my_choise, ),
+              ),
+            ],
+          ),
+          Text("Choose one of:"),
+          Row(
+            spacing: 10,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            OutlinedButton.icon(
+              // iconAlignment: IconAlignment.start,
+              style: OutlinedButton.styleFrom(
+                iconColor: appbar_bg_color,
+                foregroundColor: appbar_bg_color, 
+              ),
+              onPressed: () {compare(1);},
+              icon: Icon(Icons.landscape),
+              label: Text("Rock"),
+            ),
+            OutlinedButton.icon(
+              style: OutlinedButton.styleFrom(
+                iconColor: appbar_bg_color,
+                foregroundColor: appbar_bg_color, 
+              ),
+              onPressed: () {compare(2);},
+              icon: Icon(Icons.article),
+              label: Text("Paper"),
+            ),
+            OutlinedButton.icon(
+              style: OutlinedButton.styleFrom(
+                iconColor: appbar_bg_color,
+                foregroundColor: appbar_bg_color, 
+              ),
+              onPressed: () {compare(3);},
+              icon: Icon(Icons.cut),
+              label: Text("Scissers"),
+            ),
+          ],)
+        ],
+      ),
+      )
+    ;
+  }
+  void compare (int a){
+    var b = math.Random().nextInt(3) + 1;
+    dev.log("$b");
+    if (a == 1) {
+      setState(() {
+        user_choise = "img/landscape.png";
+      });
+      if (b == 1) {
+        setState(() {
+          tie();
+          my_choise = "img/landscape.png";
+        });
+      }else if(b == 2){
+        setState(() {
+          lose();
+          my_choise = "img/article.png";
+        });
+      }else if(b == 3){
+        setState(() {
+          win();
+          my_choise = "img/cut.png";
+        });
+      }
+    }else if(a == 2){
+      setState(() {
+        user_choise = "img/article.png";
+      });
+      if (b == 1) {
+        setState(() {
+          win();
+          my_choise = "img/landscape.png";
+        });
+      }else if(b == 2){
+        setState(() {
+          tie();
+          my_choise = "img/article.png";
+        });
+      }else if(b == 3){
+        setState(() {
+          lose();
+          my_choise = "img/cut.png";
+        });
+      }
+    }else if(a == 3){
+      setState(() {
+        user_choise = "img/cut.png";
+      });
+      if (b == 1) {
+        setState(() {
+          lose();
+          my_choise = "img/landscape.png";
+        });
+      }else if(b == 2){
+        setState(() {
+          win();
+          my_choise = "img/article.png";
+        });
+      }else if(b == 3){
+        setState(() {
+          tie();
+          my_choise = "img/cut.png";
+        });
+      }
+    }
+  }
+  void lose(){
+    setState(() {
+      result = "Lose";
+      page_bg_color = Colors.red.shade300;
+    });
+  }
+  void win(){
+    setState(() {
+      result = "Win";
+      page_bg_color= Colors.green.shade200;
+    });
+  }
+  void tie(){
+    setState(() {
+      result = "Tie";
+      page_bg_color= Colors.yellow.shade50; 
+    });
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+  
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Text("something something");
   }
+  
 }
